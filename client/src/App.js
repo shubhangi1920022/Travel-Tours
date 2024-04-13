f11325467b22e29a9e323e3a85d025eb70f28881
shubhangi1920022 <shubhangidodake76@gmail.com>
2024-04-12 23:14:50 -0700

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,18 @@ import React from 'react'
 import './style.css'
 import Home from './routes/Home';
 import {Routes,Route} from 'react-router-dom'
-import About from './routes/About';
-import Service from './routes/Service';
-import Contact from './routes/Contact';
+// import About from './routes/About';
+// import Service from './routes/Service';
+// import Contact from './routes/Contact';
 
 function App() {
   return (
     <div className="App">
     <Routes>
       <Route path='/' element={<Home/>}/>
-      <Route path='/about' element={<About/>}/>
+      {/* <Route path='/about' element={<About/>}/>
       <Route path='/service' element={<Service/>}/>
-      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/contact' element={<Contact/>}/> */}
     </Routes>
 
     </div>
